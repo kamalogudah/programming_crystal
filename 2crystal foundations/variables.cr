6d6b82108fe071c8832af9c5fab9a7041aadafd8
kamalogudah <mcpaul2058@gmail.com>
2019-09-01 16:56:16 +0300

    Using Basic Varibales and Types

---
+++ 2crystal foundations/variables.cr
@@ -0,0 +1,11 @@
+# multiple assignment
+#  name, element, hardness = "Diamond", 'C', 10
+name = "Diamond"
+element = 'C'
+hardiness : Int32
+hardness = 19
+puts typeof(name)
+puts typeof(element)
+puts typeof(hardness)
+# Constants
+DIAMOND_HARDNESS = 10
